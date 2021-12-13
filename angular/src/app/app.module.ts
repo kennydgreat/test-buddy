028d074f797994fcde18ec06e58e7dc5fb92fc80
Kenneth Uyabeme <uyabeme@sheridancollege.ca>
2021-12-13 17:47:59 -0500

    Story: https://app.asana.com/0/1174298189166817/1201467582176280/f -added app icon -added fonts -adding top nar bar and side bar no styling

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, MatSidenavModule, BrowserAnimationsModule, MatListModule, MatIconModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
