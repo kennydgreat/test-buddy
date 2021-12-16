53d38a5dc76eef0d5dfb483ba930229056feabd7
Kenneth Uyabeme <uyabeme@sheridancollege.ca>
2021-12-16 14:57:15 -0500

    Story: https://app.asana.com/0/1174298189166817/1201467582176280/f -adding mocha to fix test compile errors -adding material rounded icons -adding unit list item ui

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -7,10 +7,12 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
 import { HomeComponent } from './home/home.component';
+import {MatButtonModule} from '@angular/material/button';
+import { UnitsListItemComponent } from './units-list-item/units-list-item.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, MatSidenavModule, BrowserAnimationsModule, MatListModule, MatIconModule],
+  declarations: [AppComponent, HomeComponent, UnitsListItemComponent],
+  imports: [BrowserModule, AppRoutingModule, MatSidenavModule, BrowserAnimationsModule, MatListModule, MatIconModule, MatButtonModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ angular/src/app/units-list-item/units-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-units-list-item',
+  templateUrl: './units-list-item.component.html',
+  styleUrls: ['./units-list-item.component.scss']
+})
+export class UnitsListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
