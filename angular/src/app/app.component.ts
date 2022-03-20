89494c2c8fa885f992128cfb66a68748e77a6203
Kenneth Uyabeme <uyabeme@sheridancollege.ca>
2022-03-20 11:20:49 -0400

    Refactor nav menu item highlight logic
    This uses the routerLinkActive directive and so scss classes
    to achieve the nav menu item's highlighted when active
    behaviour
    
    Story: https://app.asana.com/0/1174298189166817/1201575773830813/f

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -8,38 +8,5 @@ import { Component, ViewEncapsulation } from '@angular/core';
 
 export class AppComponent {
 
-  homeSideNavTextClass: string = "side-menu-item";
-
-  homeSideNavIconClass: string = "side-menu-item";
-
-  settingsSideNavTextClass: string = "side-menu-item";
-
-  settingsSideNavIconClass: string = "side-menu-item";
-
-  INACTIVE_MENU_CLASS = "side-menu-item";
-  ACTIVE_MENU_CLASS = "side-menu-item-active";
-
-
-  /**
-   * change the class of the menu element passed depending on it's active state
-   * 
-   * @param  {"home"|""} element the nav element to change
-   * @param  {boolean} active whether the element is active or not
-   */
-  changeClassesBasedOnActiveFlag(element: "home" | "settings", active: boolean){
-    switch (element){
-      case "home": {
-        this.homeSideNavIconClass = active ? this.ACTIVE_MENU_CLASS : this.INACTIVE_MENU_CLASS;
-        this.homeSideNavTextClass = active ? this.ACTIVE_MENU_CLASS : this.INACTIVE_MENU_CLASS;
-        break;
-      }
-      case "settings" :{
-        this.settingsSideNavIconClass = active ? this.ACTIVE_MENU_CLASS : this.INACTIVE_MENU_CLASS;
-        this.settingsSideNavTextClass = active ? this.ACTIVE_MENU_CLASS : this.INACTIVE_MENU_CLASS;
-        break;
-      }
-    }
-  }
-
 
 }
