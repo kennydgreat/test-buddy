052ccd109c2afafb1346a7ad3e0eb4a0d8b03be3
Kenneth Uyabeme <uyabeme@sheridancollege.ca>
2022-04-23 19:47:24 -0400

    Remove leader line code -This is to keep the code clean of useless code The leader line library has not been a good solution
    Story: https://app.asana.com/0/1174298189166817/1201575773830803/f

--- angular/src/app/concept-viewer/concept-viewer.component.ts
+++ angular/src/app/concept-viewer/concept-viewer.component.ts
@@ -17,19 +17,7 @@ export class ConceptViewerComponent implements OnInit {
   @Input() statefulConcept: ConceptStateful;
   @Output() conceptChangeEvent = new EventEmitter<void>();
 
-  defaultStateCP_ID: string = '';
-  expandedStateCP_ID: string = '';
-
-
-  CONCEPT_CP_ID = "concept-CP";
-  SUB_CONCEPT_CP_ID = "subconcept-CP";
-
-  @ViewChildren('connectionPoint', { read: ElementRef }) connectionPoints: QueryList<ElementRef>;
-
   leaderLines = [];
-
-  subscription: Subscription
-
 
   conceptCPElem: ElementRef;
 
@@ -80,10 +68,6 @@ export class ConceptViewerComponent implements OnInit {
         this.showConceptExpandedCard = false;
       }
     }
-    // set the connection point id varaible, this dictates where the connection is draw from
-    this.defaultStateCP_ID = this.showConceptExpandedCard ? "" : this.CONCEPT_CP_ID;
-    this.expandedStateCP_ID = this.showConceptExpandedCard ? this.CONCEPT_CP_ID : "";
-
   }
 
   // update concept and triggers concept change event
@@ -105,61 +89,5 @@ export class ConceptViewerComponent implements OnInit {
     this.conceptChangeEvent.emit();
   }
 
-  ngAfterViewInit(): void {
 
-    this.subscription = this.connectionPoints.changes.subscribe((r) => {
-      //Draw connection lines
-      //this.drawLines();
-    });
-
-  }
-
-  ngOnDestroy() {
-    //unsubscribe to connection point selector subscription
-    if (this.addSubconcept != undefined) {
-      this.subscription.unsubscribe();
-    }
-    this.removeLines();
-  }
-
-  drawLines() {
-
-    //remove lines
-    this.removeLines();
-
-    if (this.connectionPoints == undefined) {
-      // the connectionPoints are not undefined so return
-      return;
-    }
-
-    for (var i = 0; i < this.connectionPoints.length; i++) {
-      var currentElement = this.connectionPoints.get(i);
-      if (currentElement.nativeElement != null) {
-        // look for the current concept connection point
-        if (currentElement.nativeElement.id == this.CONCEPT_CP_ID) {
-          this.conceptCPElem = currentElement;
-        }
-
-        // check that current element is one of the subconcepts and the element for the current concept connection point is set
-        if (currentElement.nativeElement.id == this.SUB_CONCEPT_CP_ID && this.conceptCPElem != undefined) {
-          // create a line from the two connection points
-          var line = new LeaderLine(this.conceptCPElem.nativeElement, LeaderLine.pointAnchor(currentElement.nativeElement, {
-            x: -10,
-            y: 0
-          }));
-
-          this.leaderLines.push(line);
-
-        }
-      }
-    }
-  }
-
-  removeLines() {
-    //remove old lines
-    this.leaderLines.forEach((line) => {
-      line.remove()
-    });
-    this.leaderLines = [];
-  }
 }
